f040d8bd2c99a8aa1a077cd6095d4f6217f3749f
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-12 14:58:57 -0300

    Commit - Exibindo o header na TableView

---
+++ HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  ErleiseViagens
+//
+//  Created by Sósthenes Oliveira Lima on 12/02/24.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
