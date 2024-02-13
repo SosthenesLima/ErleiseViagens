283febcd878fe1d05972f053ef65575915220586
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-13 19:05:59 -0300

    Commit - Iniciando com as constraints01

--- HomeTableViewHeader.swift
+++ HomeTableViewHeader.swift
@@ -12,10 +12,10 @@ class HomeTableViewHeader: UIView {
   // Sosthenes - IBOutles
 
     @IBOutlet weak var tituloLabel: UILabel!
-    
-    
     @IBOutlet weak var headerView: UIView!
-    
-    
     @IBOutlet weak var bannerImageView: UIImageView!
+    
+    func configurarView() {
+        headerView.backgroundColor =  UIColor(red: 30.0/255, green: 59.0/255, blue: 119.0/255 , alpha: 1)
+    }
 }
