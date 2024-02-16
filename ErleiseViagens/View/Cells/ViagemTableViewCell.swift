83802d7b3b04470b28dc140b3e772072e0d15d63
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-15 21:36:32 -0300

    Commit - Criando uma nova célula

---
+++ ErleiseViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  ErleiseViagens
+//
+//  Created by Sósthenes Oliveira Lima on 15/02/24.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
