223975ee01620fe921a3dcf4069efc15d6577404
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-17 21:20:30 -0300

    Commit - Utilizando mock para listar as viagens02

---
+++ ErleiseViagens/ViewModel/ViagemDestaqueViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViagemDestaqueViewModel.swift
+//  ErleiseViagens
+//
+//  Created by Sósthenes Oliveira Lima on 17/02/24.
+//
+
+import Foundation
---
+++ ErleiseViagens/ViewModel/ViagemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViagemViewModel.swift
+//  ErleiseViagens
+//
+//  Created by Sósthenes Oliveira Lima on 17/02/24.
+//
+
+import Foundation
