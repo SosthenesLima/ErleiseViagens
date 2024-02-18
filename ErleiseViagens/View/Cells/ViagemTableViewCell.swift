db459ba9018ebbbdf4a1ca44dbb8eac20c2ed36b
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-18 14:59:59 -0300

    Commit - Configurando célula da TableView

--- ErleiseViagens/View/Cells/ViagemTableViewCell.swift
+++ ErleiseViagens/View/Cells/ViagemTableViewCell.swift
@@ -24,4 +24,23 @@ class ViagemTableViewCell: UITableViewCell {
     
     @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
     
+    func configuraCelula(_ viagem: Viagem?) {
+        viagemImage.image =     UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text =  viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0)"
+        
+        let atributoString: NSMutableAttributedString =  NSMutableAttributedString(string: "R$ \(viagem?.precoSemDesconto ?? 0)")
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numeroDeDias = viagem?.diaria, let numeroDeHospedes =  viagem?.hospedes {
+            let diaria = numeroDeDias == 1 ? "Diária" : "Diárias"
+            let hospedes = numeroDeHospedes == 1 ? "Pessoa" : "Pessoas"
+            
+            diariaViagemLabel.text = "\(numeroDeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+        
+    }
+    
 }
